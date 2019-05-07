3bc0d48a9ab5302efa8686c8482767e96904ad38
Devon T. Wesley <dwesley89@gmail.com>
2019-05-07 10:14:46 -0700

    fix(exports): expose more than Documentation component
    fixes #8

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
 import Documentation from "./Documentation";
+
+export {default as ContentDescriptor} from "./ContentDescriptor/ContentDescriptor";
+export {default as ContentDescriptors} from "./ContentDescriptors/ContentDescriptors";
+export {default as Errors} from "./Errors/Errors";
+export {default as ExamplePairing} from "./ExamplePairing/ExamplePairing";
+export {default as ExamplePairings} from "./ExamplePairings/ExamplePairings";
+export {default as ExpansionTable} from "./ExpansionTable/ExpansionTable";
+export {default as Info} from "./Info/Info";
+export {default as JSONSchema} from "./JSONSchema/JSONSchema";
+export {default as Links} from "./Links/Links";
+export {default as Methods} from "./Methods/Methods";
+export {default as Params} from "./Params/Params";
+export {default as Servers} from "./Servers/Servers";
+export {default as Tags} from "./Tags/Tags";
+export {default as Documentation} from "./Documentation";
+
 export default Documentation;
