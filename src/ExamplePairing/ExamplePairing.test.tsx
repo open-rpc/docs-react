0b88b5d501006c4b5b731942cd57025d2a072168
shanejonas <jonas.shane@gmail.com>
2020-05-08 11:35:35 -0700

    fix(examplePairing): add tests for by-name and empty position

--- src/ExamplePairing/ExamplePairing.test.tsx
+++ src/ExamplePairing/ExamplePairing.test.tsx
@@ -3,6 +3,14 @@ import ReactDOM from "react-dom";
 import ExamplePairing from "./ExamplePairing";
 import examples from "@open-rpc/examples";
 import refParser from "json-schema-ref-parser";
+
+it("renders handles no examplePosition", async () => {
+  const div = document.createElement("div");
+  const simpleMath = await refParser.dereference(examples.simpleMath) as OpenrpcDocument;
+  ReactDOM.render(<ExamplePairing method={simpleMath.methods[0]} />, div);
+  expect(div.innerHTML).toBe("");
+  ReactDOM.unmountComponentAtNode(div);
+});
 import { OpenrpcDocument } from "@open-rpc/meta-schema";
 
 it("renders examples", async () => {
@@ -11,5 +19,43 @@ it("renders examples", async () => {
   ReactDOM.render(<ExamplePairing method={simpleMath.methods[0]} examplePosition={0} />, div);
   expect(div.innerHTML.includes("2")).toBe(true);
   expect(div.innerHTML.includes("4")).toBe(true);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders examples with params by-name", async () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<ExamplePairing method={{
+    examples: [
+      {
+        name: "fooExample",
+        params: [
+          {
+            name: "foo",
+            value: "bar",
+          },
+        ],
+        result: {
+          name: "exampleResultThing",
+          value: "potato",
+        },
+      },
+    ],
+    name: "myMethod",
+    paramStructure: "by-name",
+    params: [{
+      name: "foo",
+      schema: {
+        type: "string",
+      },
+    }],
+    result: {
+      name: "resultThing",
+      schema: {
+        type: "string",
+      },
+    },
+  }} examplePosition={0} />, div);
+  expect(div.innerHTML.includes("foo")).toBe(true);
+  expect(div.innerHTML.includes("bar")).toBe(true);
   ReactDOM.unmountComponentAtNode(div);
 });
