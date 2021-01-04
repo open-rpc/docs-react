70f17bca9eec15e42a97c0b16eede6dc8df9e551
shanejonas <jonas.shane@gmail.com>
2021-01-04 13:18:03 -0800

    fix: add syntax highlighted markdown tests

--- src/MarkdownDescription/MarkdownDescription.tsx
+++ src/MarkdownDescription/MarkdownDescription.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { materialDark, materialLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import ReactMarkdown from "react-markdown";
-import { Container } from "@material-ui/core";
 
 interface IProps {
   className?: string;
@@ -11,25 +10,24 @@ interface IProps {
 }
 
 const MarkdownDescription: React.FC<IProps> = ({ source, className, uiSchema }) => {
-  try {
-    return (
-      <ReactMarkdown
-        renderers={{
-          code: ({ language, value }) => {
-            return <SyntaxHighlighter
-              style={uiSchema && uiSchema.appBar["ui:darkMode"] ? materialDark : materialLight}
-              language={language}
-              children={value}
-            />;
-          },
-        }}
-        source={source}
-        className={className}
-      />
-    );
-  } catch (e) {
-    return <ReactMarkdown source={source} className={className} />;
-  }
+  return (
+    <ReactMarkdown
+      renderers={{
+        code: ({ language, value }) => {
+          if (!value) {
+            return <pre><code></code></pre>;
+          }
+          return <SyntaxHighlighter
+            style={uiSchema && uiSchema.appBar && uiSchema.appBar["ui:darkMode"] ? materialDark : materialLight}
+            language={language}
+            children={value}
+          />;
+        },
+      }}
+      source={source}
+      className={className}
+    />
+  );
 };
 
 export default MarkdownDescription;
