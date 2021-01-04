9a4faf581530e006d630a05d7d6e7d3c06dc1ae0
shanejonas <jonas.shane@gmail.com>
2021-01-04 12:06:41 -0800

    fix: use commonjs version of prism styles

--- src/MarkdownDescription/MarkdownDescription.tsx
+++ src/MarkdownDescription/MarkdownDescription.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { materialDark, materialLight } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { materialDark, materialLight } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import ReactMarkdown from "react-markdown";
 import { Container } from "@material-ui/core";
 
